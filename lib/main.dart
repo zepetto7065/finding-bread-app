58d9a95d0195554d330109390aa646954072a050
유상문 <sangmoon.yoo@wadiz.kr>
2022-08-27 00:26:03 +0900

    #10-ios/android 분기

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:finding_bread_app/root_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +15,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Finding Bread',
+    return buildApp(context);
+  }
 
-      home: Container(
+  StatefulWidget buildApp(BuildContext context) {
+      return MaterialApp(
+        title: 'Finding Bread',
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.brown, brightness: Brightness.light
+            ),
+            textTheme: GoogleFonts.juaTextTheme(
+                Theme.of(context)
+                    .textTheme
+            )
+        ),
+        home: Container(
           child: RootPage(),
-         color: Colors.white,
-      ),
-    );
+          color: Colors.white,
+        ),
+      );
   }
 }
